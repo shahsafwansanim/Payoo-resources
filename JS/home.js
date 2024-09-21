8037b2f1698ed912bc295f263cbccb96dc9ce5c9
safwan <safwan.nghs2@gmail.com.com>
2024-09-21 20:14:21 +0600

    ajj

---
+++ JS/home.js
@@ -0,0 +1,24 @@
+document.getElementById('btn-add-money')
+   .addEventListener('click',function(event){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    
+    console.log(addMoneyInput);
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(pinNumberInput);
+
+        if(pinNumberInput === '1234'){
+            console.log('adding money to your account');
+            const balance = document.getElementById('account-balance').innerText;
+             console.log(balance)
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = balanceNumber + addMoneyNumber;
+
+             console.log(newBalance)
+             document.getElementById('account-balance').innerText= newBalance;
+        }
+        else{
+            alert('Please try again')
+        }
+})
