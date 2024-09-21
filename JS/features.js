36e7c5a34a42ddf99746bf8863471cc732731e4f
safwan <safwan.nghs2@gmail.com.com>
2024-09-21 20:56:35 +0600

    all

---
+++ JS/features.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-show-cash-out')
+   .addEventListener('click',function(event){
+    event.preventDefault();
+     document.getElementById('cash-out-form').classList.remove('hidden');
+     document.getElementById('add-money-form').classList.add('hidden');
+
+   })
