ca41a8d8bcda60c2d577356aa1c9a2dda1455606
safwan <safwan.nghs2@gmail.com.com>
2024-09-21 19:41:44 +0600

    first commit

---
+++ JS/login.js
@@ -0,0 +1,17 @@
+document.getElementById('button-login')
+   .addEventListener('click', function(event){
+
+    event.preventDefault();
+
+     const phoneNumber = document.getElementById('phone-number').value;
+     const pinNumber = document.getElementById('pin-=number').value;
+      console.log(phoneNumber,pinNumber);
+      if(phoneNumber==='5' && pinNumber === '1234'){
+         console.log('you are logged in ');
+         window.location.href = '/home.html';
+
+      }
+      else{
+         alert('Wrong phone number or pin');
+      }
+   })
